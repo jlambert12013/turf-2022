7699ee3bfd8ad5de68119926a5bb2b2c20345a2e
Jim Lambert <jlambert12013@gmail.om>
2023-01-08 17:40:38 -0600

    Initial Project Setup

---
+++ turf/turfApp.swift
@@ -0,0 +1,17 @@
+//
+//  turfApp.swift
+//  turf
+//
+//  Created by Jim Lambert on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct turfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
