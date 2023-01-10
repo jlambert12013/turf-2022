0bf82c8a4704f5af8f8d108e7c078b0985c81f71
Jim Lambert <jlambert12013@gmail.com>
2023-01-10 02:43:12 -0600

    Updating

--- turf/turfApp.swift
+++ turf/turfApp.swift
@@ -1,18 +1,9 @@
-//
-//  turfApp.swift
-//  turf
-//
-//  Created by Jim Lambert on 1/8/23.
-//
-
 import SwiftUI
 import FirebaseCore
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
-        // Adding Firebase SDK
         FirebaseApp.configure()
         return true
     }
@@ -20,8 +11,6 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
 
 @main
 struct turfApp: App {
-    
-    // Adding Firebase to App Delegate
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
