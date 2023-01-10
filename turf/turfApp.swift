60af80bb38f5bdee0566e573a1c3c65f0d9e617f
Jim Lambert <jlambert12013@gmail.com>
2023-01-10 02:56:20 -0600

    Imported Firebase, Setup Config, made folders

--- turf/turfApp.swift
+++ turf/turfApp.swift
@@ -1,16 +1,16 @@
-import SwiftUI
 import FirebaseCore
+import SwiftUI
+import ColorSync
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         FirebaseApp.configure()
         return true
     }
 }
 
-@main
-struct MyApp: App {
+@main struct MyApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
